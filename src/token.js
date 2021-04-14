523d8382cdb5b23f5e45a030700af6b3c1b08748
IvanovRoman <werert87@mail.ru>
2021-04-14 08:51:51 +0300

    Returned to working condition

--- src/token.js
+++ src/token.js
@@ -12,58 +12,36 @@ var axios = require('axios-https-proxy-fix');
 /* eslint-disable */
 // BEGIN
 
-function sM(a) {
-    var b;
-    if (null !== yr)
-        b = yr;
-    else {
-        b = wr(String.fromCharCode(84));
-        var c = wr(String.fromCharCode(75));
-        b = [b(), b()];
-        b[1] = c();
-        b = (yr = window[b.join(c())] || "") || ""
-    }
-    var d = wr(String.fromCharCode(116))
-        , c = wr(String.fromCharCode(107))
-        , d = [d(), d()];
-    d[1] = c();
-    c = "&" + d.join("") + "=";
-    d = b.split(".");
-    b = Number(d[0]) || 0;
-    for (var e = [], f = 0, g = 0; g < a.length; g++) {
-        var l = a.charCodeAt(g);
-        128 > l ? e[f++] = l : (2048 > l ? e[f++] = l >> 6 | 192 : (55296 == (l & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (l = 65536 + ((l & 1023) << 10) + (a.charCodeAt(++g) & 1023),
-            e[f++] = l >> 18 | 240,
-            e[f++] = l >> 12 & 63 | 128) : e[f++] = l >> 12 | 224,
-            e[f++] = l >> 6 & 63 | 128),
-            e[f++] = l & 63 | 128)
-    }
-    a = b;
-    for (f = 0; f < e.length; f++)
-        a += e[f],
-            a = xr(a, "+-a^+6");
-    a = xr(a, "+-3^+b+-f");
-    a ^= Number(d[1]) || 0;
-    0 > a && (a = (a & 2147483647) + 2147483648);
-    a %= 1E6;
-    return c + (a.toString() + "." + (a ^ b))
-}
-
-var yr = null;
-var wr = function(a) {
-    return function() {
-        return a
-    }
-}
-    , xr = function(a, b) {
+function gq(a, b) {
     for (var c = 0; c < b.length - 2; c += 3) {
-        var d = b.charAt(c + 2)
-            , d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d)
-            , d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
+        var d = b.charAt(c + 2);
+        d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d);
+        d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
         a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
     }
     return a
-};
+}
+function hq(a, b) {
+    var c = b.split(".");
+    b = Number(c[0]) || 0;
+    for (var d = [], e = 0, f = 0; f < a.length; f++) {
+        var h = a.charCodeAt(f);
+        128 > h ? d[e++] = h : (2048 > h ? d[e++] = h >> 6 | 192 : (55296 == (h & 64512) && f + 1 < a.length && 56320 == (a.charCodeAt(f + 1) & 64512) ? (h = 65536 + ((h & 1023) << 10) + (a.charCodeAt(++f) & 1023),
+            d[e++] = h >> 18 | 240,
+            d[e++] = h >> 12 & 63 | 128) : d[e++] = h >> 12 | 224,
+            d[e++] = h >> 6 & 63 | 128),
+            d[e++] = h & 63 | 128)
+    }
+    a = b;
+    for (e = 0; e < d.length; e++)
+        a += d[e],
+            a = gq(a, "+-a^+6");
+    a = gq(a, "+-3^+b+-f");
+    a ^= Number(c[1]) || 0;
+    0 > a && (a = (a & 2147483647) + 2147483648);
+    c = a % 1E6;
+    return c.toString() + "." + (c ^ b)
+}
 
 // END
 /* eslint-enable */
@@ -73,7 +51,7 @@ var window = {
 };
 
 function updateTKK(opts) {
-    opts = opts || {tld: 'com'};
+    opts = opts || { tld: 'com' };
     return new Promise(function (resolve, reject) {
         var now = Math.floor(Date.now() / 3600000);
 
@@ -81,10 +59,11 @@ function updateTKK(opts) {
             resolve();
         } else {
             axios({
-                url: 'https://translate.google.' + opts.tld,
+                // url: 'https://translate.google.' + opts.tld,
+                url: 'https://translate.google.' + opts.tld + '/translate_a/element.js?cb=gtElInit&client=wt',
                 proxy: opts.proxy,
             }).then(function (res) {
-                var matches = res.data.match(/tkk:\s?'(.+?)'/i);
+                var matches = res.data.match(/tkk='(.+?)'/)
 
                 if (matches) {
                     window.TKK = matches[1];
@@ -107,10 +86,10 @@ function updateTKK(opts) {
 }
 
 function get(text, opts) {
-    return updateTKK(opts).then(function () {
-        var tk = sM(text);
+    return updateTKK(opts).then(() => {
+        var tk = hq(text, window.TKK);
         tk = tk.replace('&tk=', '');
-        return {name: 'tk', value: tk};
+        return { name: 'tk', value: tk };
     }).catch(function (err) {
         throw err;
     });
--- src/translate.ts
+++ src/translate.ts
@@ -1,76 +1,77 @@
-const translateToken = require('./token');
-import axios, { AxiosRequestConfig } from 'axios-https-proxy-fix';
-import { arrayStringify } from './util';
-import { Options } from './index';
-import { isSupport, getCode } from './language';
+const translateToken = require("./token");
+import axios, { AxiosRequestConfig } from "axios-https-proxy-fix";
+import { arrayStringify } from "./util";
+import { Options } from "./index";
+import { isSupport, getCode } from "./language";
 
 function handletranslate(data: string[], extra: Options): Promise<any> {
   let e: any;
-  if(extra.from) {
-    if(!isSupport(extra.from)) {
+  if (extra.from) {
+    if (!isSupport(extra.from)) {
       e = new Error();
       e.language = extra.from;
     }
   }
-  if(!isSupport(extra.to)) {
+  if (!isSupport(extra.to)) {
     e = new Error();
     e.language = extra.to;
   }
   if (e) {
     e.code = 400;
-    e.message = 'The language \'' + e.language + '\' is not supported';
+    e.message = "The language '" + e.language + "' is not supported";
     return new Promise(function (_, reject) {
-        reject(e);
+      reject(e);
     });
   }
   return translateToken
-    .get(data.join(''), {
-      tld: extra.tld || 'com',
+    .get(data.join(""), {
+      tld: extra.tld || "com",
       proxy: extra.proxy || false,
     })
-    .then(res => {
+    .then((res) => {
       const query = {
         anno: 3,
-        client: "webapp",
+        client: "wt_lib",
         format: extra.format,
         v: 1.0,
         key: null,
-        logld: "vTE_20190506_00",
-        sl: extra.from || 'auto',
-        tl: extra.to || 'zh-CN',
-        hl: 'zh-CN',
+        logld: "vTE_20210224_00",
+        sl: extra.from || "auto",
+        tl: extra.to || "zh-CN",
+        hl: "zh-CN",
         sp: "nmt",
-        tc: 2,
+        tc: 1,
         sr: 1,
         tk: res.value,
-        mode: 1
+        mode: 1,
       };
 
       const options: AxiosRequestConfig = {
         method: "POST",
         headers: {
           "content-type": "application/x-www-form-urlencoded",
-          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
-          "Accept": "application/json, text/plain, */*",
-          'X-Requested-With': 'XMLHttpRequest'
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+          Accept: "application/json, text/plain, */*",
+          "X-Requested-With": "XMLHttpRequest",
         },
         data: arrayStringify(data),
-        url: '/translate_a/t',
+        url: "/translate_a/t",
         baseURL: `https://translate.googleapis.com`,
         params: query,
         proxy: extra.proxy || false,
-        ...(extra.config)
+        ...extra.config,
       };
-      let browersUrl = 'https://cors-anywhere.herokuapp.com/';
+      let browersUrl = "https://cors-anywhere.herokuapp.com/";
       if (extra.browersUrl) {
         browersUrl = extra.browersUrl;
       }
-      if(extra.browers) {
+      if (extra.browers) {
         options.baseURL = browersUrl + options.baseURL;
       }
 
       return axios(options);
-    })
+    });
 }
 
 export default handletranslate;
